56136da0f3bcf869629571f092c90e76e6376d3d
Gustavo Pedroso Gal <20181PF.CC0159@passofundo.ifsul.local>
2022-09-30 09:14:33 -0300

    teste

--- rotas.js
+++ rotas.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 
 const controlePredios = require('./controladores/predios');
-
+const controleSalas = require("./controladores/salas");
 const rotas = new Router();
 
 rotas.route('/predios')
@@ -12,5 +12,14 @@ rotas.route('/predios')
 rotas.route('/predios/:codigo')
      .get(controlePredios.getPredioPorCodigo)
      .delete(controlePredios.deletePredio)
+
+rotas.route('/salas')
+   .get(controleSalas.getSalas)
+   .post(controleSalas.addSala)
+   .put(controleSalas.updateSala)
+
+rotas.route('/salas/:codigo')
+   .get(controleSalas.getSalaPorCodigo)
+   .delete(controleSalas.deleteSala)
 
 module.exports = rotas;
